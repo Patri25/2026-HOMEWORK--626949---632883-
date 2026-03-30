56fe329ef13b5e7ea3f116ea8421f9d65cab989b
patri <97561814+Patri25@users.noreply.github.com>
2026-03-30 23:33:53 +0200

    Add files via upload
    HW1

---
+++ diadia/src/HM1/PartitaTest.java
@@ -0,0 +1,14 @@
+package HM1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PartitaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
