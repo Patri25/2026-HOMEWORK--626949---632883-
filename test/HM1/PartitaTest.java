2eb620bf03e7e2e638490d2d43628cc657eaec36
patri <pasciscio@gmail.com>
2026-04-01 13:53:00 +0200

    Update PartitaTest.java
    Aggiunti i 3 test per il metodo "Vinta()".
    Da implementare "IsFinita"...

--- test/HM1/PartitaTest.java
+++ test/HM1/PartitaTest.java
@@ -2,14 +2,44 @@ package HM1;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PartitaTest {
-	//provsgithub
+	
+	private Partita partita;
+	private Stanza stanzaVincente;
+	private Stanza stanzaCorrente;
+	
+	
+	@BeforeEach
+	public void setUp() {
+		partita = new Partita();
+		stanzaVincente=this.partita.getStanzaVincente();
+		stanzaCorrente=this.partita.getStanzaCorrente();
+	}
+	
+
+	//---------------------VINTA, I 3 TEST------------------------
 
 	@Test
-	void test() {
-		fail("Not yet implemented daje roma");
+	void testVintaCorrispondenteAllaVittoria() {
+		this.partita.setStanzaCorrente(stanzaVincente);
+		assertTrue(this.partita.vinta());
 	}
+	
+	@Test
+	void testVintaNonCorrispondenteAllaVittoria() {
+		assertFalse(this.partita.vinta());
+	}
+	
+	@Test
+	void testVintaAncheSeNonStanzaInizialeMaNonCorretta() {
+		Stanza stanzaCasuale=new Stanza("NInesistente");
+		this.partita.setStanzaCorrente(stanzaCasuale);
+		assertFalse(this.partita.vinta());
+	}
+	
+	//---------------------ISFINITA, I 3 TEST------------------------
 
 }
