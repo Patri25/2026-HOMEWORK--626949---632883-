1310b7620764c52bb278088372615e19864a18c2
FilippoCasagrande <filicasa1@gmail.com>
2026-04-01 12:41:38 +0200

    dggfddg
    dsggf

--- test/HM1/PartitaTest.java
+++ test/HM1/PartitaTest.java
@@ -5,10 +5,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class PartitaTest {
+	//provsgithub
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		fail("Not yet implemented daje roma");
 	}
 
 }
