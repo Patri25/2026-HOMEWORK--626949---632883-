b7751a52b6b0d32220d64b91f970286d7ca7394b
patri <pasciscio@gmail.com>
2026-05-05 12:27:52 +0200

    Esercizio 6

--- src/it/uniroma3/diadia/ambienti/StanzaBuia.java
+++ src/it/uniroma3/diadia/ambienti/StanzaBuia.java
@@ -1,5 +1,31 @@
 package it.uniroma3.diadia.ambienti;
 
-public class StanzaBuia {
+public class StanzaBuia extends Stanza {
+	
+	
+	private String attrezzoSpecifico;
+	final static String ATTREZZO_SPECIFICO_DEFAULT="torcia";
+
+	public StanzaBuia(String nome, String attrezzoPerVedere) {
+		super(nome);
+		this.attrezzoSpecifico=attrezzoPerVedere;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public StanzaBuia(String nome) {
+		this(nome, ATTREZZO_SPECIFICO_DEFAULT);
+	}
+	
+	@Override
+	public String getDescrizione() {
+		if(this.hasAttrezzo(attrezzoSpecifico)) {
+			return super.getDescrizione();
+		}
+		else {
+			return ("qui c'è buio pesto...");
+		}
+		
+		
+    }
 
 }
